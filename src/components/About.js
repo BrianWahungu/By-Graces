a1c28c20d6e91b032de7c47040c3adb64a72a1a4
zaiky09 <zamilmozamil@outlook.com>
2023-01-27 12:55:16 +0300

    changed spelling

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,7 @@ function About(){
       {/* <center><h1>About Us</h1></center>  */}
       <div className="para">
         <center>
-        <p><strong>Our charity organization, <em>By Graces</em>, has a rich history of serving the community and making a positive impact in the lives of those in need. Founded over 100 years ago, XYZ has always been dedicated to improving the lives of those less fortunate by providing essential services such as food, housing, and medical assistance.<br></br>
+        <p><strong>Our charity organization, <em>By Graces</em>, has a rich history of serving the community and making a positive impact in the lives of those in need. Founded over 100 years ago, By Graces has always been dedicated to improving the lives of those less fortunate by providing essential services such as food, housing, and medical assistance.<br></br>
           <br></br>Through the years, we have adapted to the changing needs of the community and have expanded our services to include education and job training programs, as well as mental health support. We believe that by providing a comprehensive approach to addressing poverty, we can truly make a lasting difference in the lives of those we serve.<br></br>
           <br></br>Our dedicated team of staff and volunteers work tirelessly to ensure that our programs are effective and that the needs of the community are met. We are proud of the impact that we have made over the years and are committed to continuing our mission for many years to come.
             If you are interested in joining our cause and making a difference in the lives of those in need, we welcome you to get involved. We are always in need of volunteers and donations to support our programs and services.</strong>
